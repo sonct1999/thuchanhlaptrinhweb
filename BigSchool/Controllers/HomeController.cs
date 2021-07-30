3f6788429a716bf6cf4103acdb956b9a365634e5
Sonct99 <btht.pxs@gmail.com>
2021-07-30 13:18:07 +0700

    PhanXuanSon

---
+++ BigSchool/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class HomeController : Controller
+    {
+        BigSchoolDB con = new BigSchoolDB();
+        public ActionResult Index()
+        {
+            
+            var upcommingCourse = con.Courses.Where(p => p.Datetime >
+            DateTime.Now).OrderBy(p => p.Datetime).ToList();
+
+            var userID = User.Identity.GetUserId();
+            foreach (Course i in upcommingCourse)
+
+            {
+                ApplicationUser user =
+
+                System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>(
+                ).FindById(i.LectureId);
+                i.Name = user.Name;
+
+                if (userID != null)
+
+                {
+                    i.isLogin = true;
+
+                    Attendance find = con.Attendances.FirstOrDefault(p =>
+
+                    p.CourseId == i.Id && p.Attendee == userID);
+                    if (find == null)
+                        i.isShowGoing = true;
+
+                    Following findFollow = con.Followings.FirstOrDefault(p =>
+
+                    p.FollowerId == userID && p.FolloweeId == i.LectureId);
+
+                    if (findFollow == null)
+                        i.isShowFollow = true;
+                }
+            }
+            return View(upcommingCourse);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
