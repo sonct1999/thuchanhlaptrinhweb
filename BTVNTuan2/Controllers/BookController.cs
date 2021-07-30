d3a6dbf0e86a4e3d9eeec7ff4a3e0aad4ce16d72
Sonct99 <btht.pxs@gmail.com>
2021-07-30 13:32:17 +0700

    BTVNTuan2

---
+++ BTVNTuan2/Controllers/BookController.cs
@@ -0,0 +1,137 @@
+﻿using BTVNTuan2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTVNTuan2.Controllers
+{
+    public class BookController : Controller
+    {
+        public ActionResult ListBookModel()
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Nguyễn Du", "Truyện Thúy Kiều (Bản Đặc Biệt)", "Truyện Kiều của Nguyễn Du đã hiện diện trong đời sống của dân tộc Việt Nam ta và trở thành một phần quan trọng làm nên vẻ đẹp của tâm hồn Việt, tinh hoa văn hóa Việt.", "/Content/images/1.png", "90000"));
+            books.Add(new Book(2, "Vũ Ngọc Phan", "Tục Ngữ, Ca Dao, Dân Ca Việt Nam (Tái Bản 2020)", "Tác phẩm của nhà nghiên cứu văn học Vũ Ngọc Phan được trao tặng Giải thưởng Hồ Chí Minh đợt đầu tiên (năm1996, lĩnh vực văn nghệ dân gian.", "/Content/images/2.png", "105000"));
+            books.Add(new Book(3, "Hồ Chí Minh", "Nhật Ký Trong Tù", "“Tập thơ Nhật ký trong tùcủa Hồ Chí Minh là một kho tàng về biết bao khía cạnh của cuộc đời, con người và nghệ thuật mà sự phong phú còn cần được tiếp tục nghiên cứu.” - THỦ TƯỚNG PHẠM VĂN ĐỒNG", "/Content/images/3.png", "65000"));
+            return View(books);
+        }
+        public ActionResult EditBook(int id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Nguyễn Du", "Truyện Thúy Kiều (Bản Đặc Biệt)", "Truyện Kiều của Nguyễn Du đã hiện diện trong đời sống của dân tộc Việt Nam ta và trở thành một phần quan trọng làm nên vẻ đẹp của tâm hồn Việt, tinh hoa văn hóa Việt.", "/Content/images/1.png", "90000"));
+            books.Add(new Book(2, "Vũ Ngọc Phan", "Tục Ngữ, Ca Dao, Dân Ca Việt Nam (Tái Bản 2020)", "Tác phẩm của nhà nghiên cứu văn học Vũ Ngọc Phan được trao tặng Giải thưởng Hồ Chí Minh đợt đầu tiên (năm1996, lĩnh vực văn nghệ dân gian.", "/Content/images/2.png", "105000"));
+            books.Add(new Book(3, "Hồ Chí Minh", "Nhật Ký Trong Tù", "“Tập thơ Nhật ký trong tùcủa Hồ Chí Minh là một kho tàng về biết bao khía cạnh của cuộc đời, con người và nghệ thuật mà sự phong phú còn cần được tiếp tục nghiên cứu.” - THỦ TƯỚNG PHẠM VĂN ĐỒNG", "/Content/images/3.png", "65000"));
+            Book book = new Book();
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    book = b;
+                    break;
+                }
+            }
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [HttpPost, ActionName("EditBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditBook(int id, string Author, string Title, string Description, string ImageCover, string Price)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Nguyễn Du", "Truyện Thúy Kiều (Bản Đặc Biệt)", "Truyện Kiều của Nguyễn Du đã hiện diện trong đời sống của dân tộc Việt Nam ta và trở thành một phần quan trọng làm nên vẻ đẹp của tâm hồn Việt, tinh hoa văn hóa Việt.", "/Content/images/1.png", "90000"));
+            books.Add(new Book(2, "Vũ Ngọc Phan", "Tục Ngữ, Ca Dao, Dân Ca Việt Nam (Tái Bản 2020)", "Tác phẩm của nhà nghiên cứu văn học Vũ Ngọc Phan được trao tặng Giải thưởng Hồ Chí Minh đợt đầu tiên (năm1996, lĩnh vực văn nghệ dân gian.", "/Content/images/2.png", "105000"));
+            books.Add(new Book(3, "Hồ Chí Minh", "Nhật Ký Trong Tù", "“Tập thơ Nhật ký trong tùcủa Hồ Chí Minh là một kho tàng về biết bao khía cạnh của cuộc đời, con người và nghệ thuật mà sự phong phú còn cần được tiếp tục nghiên cứu.” - THỦ TƯỚNG PHẠM VĂN ĐỒNG", "/Content/images/3.png", "65000"));
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    b.Description = Description;
+                    b.Price = Price;
+                    b.Title = Title;
+                    b.Author = Author;
+                    b.ImageCover = ImageCover;
+                    break;
+                }
+            }
+            return View("ListBookModel", books);
+        }
+        public ActionResult CreateBook()
+        {
+            return View();
+        }
+        [HttpPost, ActionName("CreateBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contract([Bind(Include = "Id, Author, Title, Description,ImageCover, Price")] Book book)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Nguyễn Du", "Truyện Thúy Kiều (Bản Đặc Biệt)", "Truyện Kiều của Nguyễn Du đã hiện diện trong đời sống của dân tộc Việt Nam ta và trở thành một phần quan trọng làm nên vẻ đẹp của tâm hồn Việt, tinh hoa văn hóa Việt.", "/Content/images/1.png", "90000"));
+            books.Add(new Book(2, "Vũ Ngọc Phan", "Tục Ngữ, Ca Dao, Dân Ca Việt Nam (Tái Bản 2020)", "Tác phẩm của nhà nghiên cứu văn học Vũ Ngọc Phan được trao tặng Giải thưởng Hồ Chí Minh đợt đầu tiên (năm1996, lĩnh vực văn nghệ dân gian.", "/Content/images/2.png", "105000"));
+            books.Add(new Book(3, "Hồ Chí Minh", "Nhật Ký Trong Tù", "“Tập thơ Nhật ký trong tùcủa Hồ Chí Minh là một kho tàng về biết bao khía cạnh của cuộc đời, con người và nghệ thuật mà sự phong phú còn cần được tiếp tục nghiên cứu.” - THỦ TƯỚNG PHẠM VĂN ĐỒNG", "/Content/images/3.png", "65000"));
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    books.Add(book);
+                }
+            }
+            catch (RetryLimitExceededException)
+            {
+                ModelState.AddModelError("", "Error Save Data");
+            }
+            return View("ListBookModel", books);
+        }
+        public ActionResult Delete(int id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Nguyễn Du", "Truyện Thúy Kiều (Bản Đặc Biệt)", "Truyện Kiều của Nguyễn Du đã hiện diện trong đời sống của dân tộc Việt Nam ta và trở thành một phần quan trọng làm nên vẻ đẹp của tâm hồn Việt, tinh hoa văn hóa Việt.", "/Content/images/1.png", "90000"));
+            books.Add(new Book(2, "Vũ Ngọc Phan", "Tục Ngữ, Ca Dao, Dân Ca Việt Nam (Tái Bản 2020)", "Tác phẩm của nhà nghiên cứu văn học Vũ Ngọc Phan được trao tặng Giải thưởng Hồ Chí Minh đợt đầu tiên (năm1996, lĩnh vực văn nghệ dân gian.", "/Content/images/2.png", "105000"));
+            books.Add(new Book(3, "Hồ Chí Minh", "Nhật Ký Trong Tù", "“Tập thơ Nhật ký trong tùcủa Hồ Chí Minh là một kho tàng về biết bao khía cạnh của cuộc đời, con người và nghệ thuật mà sự phong phú còn cần được tiếp tục nghiên cứu.” - THỦ TƯỚNG PHẠM VĂN ĐỒNG", "/Content/images/3.png", "65000"));
+            Book book = new Book();
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    book = b;
+                    break;
+                }
+            }
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, string Author, string Title, string Description, string ImageCover, string Price)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Nguyễn Du", "Truyện Thúy Kiều (Bản Đặc Biệt)", "Truyện Kiều của Nguyễn Du đã hiện diện trong đời sống của dân tộc Việt Nam ta và trở thành một phần quan trọng làm nên vẻ đẹp của tâm hồn Việt, tinh hoa văn hóa Việt.", "/Content/images/1.png", "90000"));
+            books.Add(new Book(2, "Vũ Ngọc Phan", "Tục Ngữ, Ca Dao, Dân Ca Việt Nam (Tái Bản 2020)", "Tác phẩm của nhà nghiên cứu văn học Vũ Ngọc Phan được trao tặng Giải thưởng Hồ Chí Minh đợt đầu tiên (năm1996, lĩnh vực văn nghệ dân gian.", "/Content/images/2.png", "105000"));
+            books.Add(new Book(3, "Hồ Chí Minh", "Nhật Ký Trong Tù", "“Tập thơ Nhật ký trong tùcủa Hồ Chí Minh là một kho tàng về biết bao khía cạnh của cuộc đời, con người và nghệ thuật mà sự phong phú còn cần được tiếp tục nghiên cứu.” - THỦ TƯỚNG PHẠM VĂN ĐỒNG", "/Content/images/3.png", "65000"));
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    books.Remove(b);
+                    break;
+                }
+            }
+            return View("ListBookModel", books);
+        }
+    }
+}
---
+++ BTVNTuan2/Models/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BTVNTuan2.Models
+{
+    public class Book
+    {
+        private int id;
+        private string author;
+        private string title;
+        private string description;
+        private string image_cover;
+        private string price;
+
+        public Book()
+        {
+
+        }
+
+        public Book(int id, string author, string title, string description, string image_cover, string price)
+        {
+            this.id = id;
+            this.author = author;
+            this.title = title;
+            this.description = description;
+            this.image_cover = image_cover;
+            this.price = price;
+        }
+        [Display(Name = "Mã sách")]
+        public int Id { get => id; set => id = value; }
+        [Required(ErrorMessage = "Tác giả không được để trống")]
+        [StringLength(30, ErrorMessage = "Tác giả sách không được vượt quá 30 ký tự")]
+        [Display(Name = "Tác giả")]
+        public string Author { get => author; set => author = value; }
+        [Required(ErrorMessage = "Tựa sách không được để trống")]
+        [StringLength(30, ErrorMessage = "Tựa sách không được vượt quá 100 ký tự")]
+        [Display(Name = "Tựa sách")]
+        public string Title { get => title; set => title = value; }
+        [Display(Name = "Miêu tả")]
+        public string Description { get => description; set => description = value; }
+        [Display(Name = "Hình ảnh")]
+        public string ImageCover { get => image_cover; set => image_cover = value; }
+        [Required(ErrorMessage = "Giá không được để trống")]
+        [Range(1000, 1000000, ErrorMessage = "Giá sách từ 1000 - 1.000.000")]
+        [Display(Name = "Giá sách")]
+        public string Price { get => price; set => price = value; }
+    }
+}
